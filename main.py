b775aaf81c0ef3b342de62c847a37072acd1fa10
loxx <112658304+loxxit@users.noreply.github.com>
2023-11-04 13:21:32 +0300

    Maked an adv status/update checker

--- main.py
+++ main.py
@@ -1,73 +1,109 @@
 import os
 try:
-    import requests
+    import discord
     import time
     import json
-    import logging
-    from discord import Status
-    import discord
-    from rgbprint import gradient_print, Color
-    from discord.ext import commands, tasks
-    import asyncio
+    import requests
+    from discord.ext import tasks, commands
+    ROBLOX_CLIENT_VERSION_API = "https://setup.rbxcdn.com/version"
+    try:
+        with open('config.json', 'r') as config_file:
+            config = json.load(config_file)
+        bot_token = config.get('bot_token')
+        if not bot_token:
+            raise ValueError("Bot token is not found in config.json")
+    except FileNotFoundError:
+        raise FileNotFoundError("config.json file not found")
+
+    secs = 5
+    with open('config.json', 'r') as config_file:
+            config = json.load(config_file)
+    secs = config.get('secs')
+    print(f"will check every {secs} secs")
+    url = "https://www.roblox.com/home"
+    url2 = "https://status.roblox.com/"
+    channel_id = ""
+    with open('config.json', 'r') as config_file:
+            config = json.load(config_file)
+    channel_id = config.get('channel_id')
+    intents = discord.Intents.default()
+    intents.typing = False
+    intents.presences = False
+    bot = commands.Bot(command_prefix="px-", intents=intents)
+
+    @bot.event
+    async def on_ready():
+        print(f'Logged in as {bot.user.name}')
+        channel = bot.get_channel(channel_id)
+        activity = discord.Streaming(name="Shinsenkyo || Loxxit",url="https://github.com/loxxit")
+        messages = []
+
+        async for message in channel.history(limit=10):
+            messages.append(message)
+
+        bot_message_exists = any(message.author == bot.user for message in messages)
+
+        if not bot_message_exists:
+            await channel.send("No bot message found. Sending a new message.")
+            update_message.start()
+
+        await bot.change_presence(activity=activity, status=discord.Status.do_not_disturb)
+
+        if bot_message_exists:
+            update_message.start()
+    @tasks.loop(seconds=secs)
+    async def update_message():
+        channel = bot.get_channel(channel_id)
+        try:
+            response = requests.get(url)
+            response2 = requests.get(url2)
+            response = requests.get(ROBLOX_CLIENT_VERSION_API)
+            new_version = response.text.strip()
+            print("checking...")
+            if response.status_code == 200:       
+                if "Active Incident" in response2.text:
+                    print("200 but sd/ai")
+                    message = "The website is up and running (Status Code 200) [Active Incident]"
+                    color = 0xFFA500 
+                else:
+                    print("succes status two hundred")
+                    message = "The website is up and running (Status Code 200)"
+                    color = 0x00FF00 
+            elif response.status_code == 503:
+                print("succes status 503 SU")
+                message = "SU (Service **Unavailable**)"
+                color = 0xFF0000
+            else:
+                print(f"succes status {response.status_code}")
+                message = f"Status Code: **{response.status_code}**"
+                color = 0x000000 
+
+            embed = discord.Embed(title=f"", description=message, color=color)
+            last_updated = time.strftime("%d.%m.%Y %H:%M (GMT+3)", time.localtime())
+            embed.set_footer(text=f"Last Updated: {last_updated}")
+            embed.set_author(name="Roblox Status",url="https://status.roblox.com/",icon_url="https://devforum-uploads.s3.dualstack.us-east-2.amazonaws.com/uploads/original/4X/0/e/e/0eeeb19633422b1241f4306419a0f15f39d58de9.png")
+            embed.add_field(name="Client Version",value=new_version,inline=True)
+            
+            if response.status_code == 503:
+                embed.add_field(name="Roblox Status",value="**503**, **SU**",inline=True)
+            elif response.status_code == 200:
+                if "Active Incident" in response2.text:
+                    embed.add_field(name="Roblox Status",value="200, Active Incident",inline=True)
+                else:
+                    embed.add_field(name="Roblox Status",value="200",inline=True)
+            else:
+                    embed.add_field(name="Roblox Status",value=f"{response.status_code}",inline=True)
+            async for message in channel.history(limit=10):
+                if message.author == bot.user:
+                    await message.edit(content="", embed=embed)
+                    break
+
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+
+    bot.run(bot_token)
 except ModuleNotFoundError as e:
     print(f"Missing module: {e.name}")
     with open("requirements.txt") as f:
         modules = f.read().splitlines()
         os.system(f"pip install {' '.join(modules)}")
-
-logging.basicConfig(level=logging.INFO)
-logging.getLogger('discord.client').setLevel(logging.WARNING)
-logging.getLogger('discord.gateway').setLevel(logging.WARNING)
-
-os.system("color c")
-
-with open("config.json") as f:
-    config = json.load(f)
-
-DISCORD_TOKEN = config["token"]
-DISCORD_CHANNEL_ID = config["id"]
-
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-
-ROBLOX_CLIENT_VERSION_API = "https://setup.rbxcdn.com/version"
-
-current_version = config["current_version"]
-last_checked = None
-task = None
-current_task = None
-
-@tasks.loop(seconds=1)
-async def check_for_updates():
-    global current_version, last_checked, task
-    response = requests.get(ROBLOX_CLIENT_VERSION_API)
-    new_version = response.text.strip()
-    if new_version != current_version:
-        config["current_version"] = new_version
-        task = "new"
-        embed = discord.Embed(title='Roblox Updated', color=0xFF0000)
-        embed.add_field(name='', value=f'**{new_version}** Old Version: **{current_version}**', inline=False)
-        embed.set_footer(text=f'Last checked: {time.strftime("%m/%d/%Y %I:%M %p")}')
-        last_checked = time.time()
-
-        
-        with open("config.json", "w") as f:
-            json.dump(config, f, indent=4)
-        current_version = new_version
-
-        channel = client.get_channel(DISCORD_CHANNEL_ID)
-        task = "embed"
-        await channel.send(embed=embed)
-
-        gradient_print(f"Update found: {new_version} - Version updated in config.json", start_color=Color.light_green, end_color=Color.pale_turquoise)
-    else:
-        if task != "checking":
-            task = "checking"
-            gradient_print("No updates found - Checking for updates.", start_color=Color.purple, end_color=Color.khaki)
-
-@client.event
-async def on_ready():
-    await client.change_presence(activity=discord.Game(name=config["activity"]), status=config["status"])
-    await check_for_updates.start()
-
-client.run(DISCORD_TOKEN)
